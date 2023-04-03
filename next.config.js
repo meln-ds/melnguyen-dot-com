5e1f6f82b101a180400f570de63201a29c4d2834
Mel Nguyen (she/her) <12276986+meln-ds@users.noreply.github.com>
2023-04-03 22:10:27 +0800

    add code to create sitemap

--- next.config.js
+++ next.config.js
@@ -9,3 +9,13 @@ module.exports = {
     ignoreDuringBuilds: true
   }
 }
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
+}
